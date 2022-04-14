c337c42d7e976faa862b92e481b16b958b387694
LucassPimentel <lpimentel32@hotmail.com>
2022-04-14 14:48:26 -0300

    pratica de variaveis

--- Aula 2 - Variáveis/Program.cs
+++ Aula 2 - Variáveis/Program.cs
@@ -10,10 +10,39 @@ namespace _2___Variaveis
     {
         static void Main(string[] args)
         {
-            int idade;
-            idade = 19;
+            // int para inteiros
+            int idade = 19;
+
+            // long 
+            long universo = 130000000000;
+
+            // short
+            short minhaContaBancaria = 200;
+
+            // float
+            float altura = 1.8f;
+
+            // double numeros flutuantes
+            double salario;
+            salario = 1364.50;
+
+            // char 
+            char inicialDoNome = 'L';
+
+            // string
+
+            string cursosProgramacao =
+@".NET
+Python
+Javascript";
+
+
+            Console.WriteLine(cursosProgramacao);
+             
             Console.Write("Executando projeto 2 - Criando variáveis");
-            Console.WriteLine("Sua idade é" + idade);
+
+            Console.WriteLine(" Sua idade é " + idade + " anos, " + "sua altura é " + altura + "m ," + "seu salário é " + salario + ", você está inserido em um universo de " + universo + " bilhões de anos" + " e seu amigo Lucas tem " + minhaContaBancaria + ",00 reais na conta.");
+
             Console.WriteLine("Execução finalizada, tecle enter para sair...");
             Console.ReadLine();
         }
