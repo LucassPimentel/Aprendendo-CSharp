c0103114521cb0e1f95a6618c316f66b0e92492d
LucassPimentel <lpimentel32@hotmail.com>
2022-04-14 10:11:00 -0300

    Hello World C#

---
+++ Aula - 1 - Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo! Essa é a minha primeira linha em c#");
+            Console.WriteLine("Aperte Enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
