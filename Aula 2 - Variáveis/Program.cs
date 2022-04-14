d98af576b23f81a25846a724eaa1570538f24570
LucassPimentel <lpimentel32@hotmail.com>
2022-04-14 10:47:27 -0300

    Aula 2 variaveis

---
+++ Aula 2 - Variáveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            idade = 19;
+            Console.Write("Executando projeto 2 - Criando variáveis");
+            Console.WriteLine("Sua idade é" + idade);
+            Console.WriteLine("Execução finalizada, tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
